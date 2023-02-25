8138a162a0cb7fa01caf1d9c848aa82c775ad694
nmnmj <nmnjay@gmail.com>
2023-02-25 14:31:18 +0530

    nnnn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import connectDB from "./db/mongoconnectin.js"
 import web from './routes/web.js'
 const app = express()
 app.use(express.json())
-
+// app.use(express.urlencoded({extended:false}))
 connectDB("mongodb+srv://naman:nmnjay@cluster1.siszk7a.mongodb.net/?retryWrites=true&w=majority")
 app.use("/", web)
 
--- routes/web.js
+++ routes/web.js
@@ -3,9 +3,58 @@ import studentModel from '../models/Students.js'
 const router = express.Router()
 
 router.get("/", async (req, res)=>{
-    const r = await studentModel.find()
-    res.send(r)
+    try {
+        const r = await studentModel.find()
+        res.send(r)
+        
+    } catch (error) {
+        console.log(error)
+    }
     
+})
+router.post("/", async (req, res)=>{
+    try {
+        const doc = await studentModel.create({
+            name:req.body.name,
+            age:req.body.age,
+            fee:req.body.fee
+        })
+        const r = await doc.save()
+        res.status(201).send(r)
+        
+    } catch (error) {
+        console.log(error)
+    }
+    
+    
+})
+
+router.get("/:id", async (req, res)=>{
+    try {
+        const r = await studentModel.findById(req.params.id)
+        res.send(r)
+        
+    } catch (error) {
+        console.log(error)
+    }
+})
+router.delete("/:id", async (req, res)=>{
+    try {
+        const r = await studentModel.findByIdAndDelete(req.params.id)
+        res.status(204).send(r)
+        
+    } catch (error) {
+        console.log(error)
+    }
+})
+router.put("/:id", async (req, res)=>{
+    try {
+        const r = await studentModel.findByIdAndUpdate(req.params.id, req.body, {returnDocument:"after"})
+        res.send(r)
+        
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 export default router
