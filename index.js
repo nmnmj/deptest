7eedb2377707d6018b3318d624869d2be0334d06
nmnmj <nmnjay@gmail.com>
2023-02-25 14:48:05 +0530

    nnnnn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express"
+import dotenv from 'dotenv'
+dotenv.config()
 import connectDB from "./db/mongoconnectin.js"
 import web from './routes/web.js'
 const app = express()
 app.use(express.json())
 // app.use(express.urlencoded({extended:false}))
-connectDB("mongodb+srv://naman:nmnjay@cluster1.siszk7a.mongodb.net/?retryWrites=true&w=majority")
+connectDB(process.env.db)
 app.use("/", web)
 
 app.listen(3000, ()=>{
