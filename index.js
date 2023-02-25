c039d5d7f67d1fbdcdbcef90e8f6665a9d5f220c
nmnmj <nmnjay@gmail.com>
2023-02-25 15:12:53 +0530

    nnnnn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import express from "express"
 import dotenv from 'dotenv'
 dotenv.config()
+import express from "express"
 import connectDB from "./db/mongoconnectin.js"
 import web from './routes/web.js'
 const app = express()
 app.use(express.json())
 // app.use(express.urlencoded({extended:false}))
-connectDB(process.env.db)
+connectDB(process.env.DB_URL)
 app.use("/", web)
 
 app.listen(3000, ()=>{
-    console.log("port" + process.env.db)
+    console.log("port" + process.env.DB_URL)
 })
